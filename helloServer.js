49aea19e1092abb7e03145833cc6ef9cf3a5c797
Lokesh Kumar <neolokeshkr@gmail.com>
2021-09-07 19:36:20 +0530

    .gitignore created and nde_modules ignored

--- helloServer.js
+++ helloServer.js
@@ -11,16 +11,30 @@ Whenever a new request is received, the request event is called,
 providing two objects: a request (an http.IncomingMessage object)
 and a response (an http.ServerResponse object).
 */
-
+console.time(server);
 const server = http.createServer((req,res) => {
     res.statusCode = 200
     res.setHeader('Content-Type', 'text/plain')
     res.end('Hello World\n')
 })
-
+console.timeEnd(server);
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
     //console.log(process);
-    console.log(process.env);
+    //console.log(process.env);
     // console.log(process.env.NODE_ENV);
 })
+
+console.log(process.env.USER_ID);
+
+process.argv.forEach((val, index) => {
+   console.log(`${index}: ${val}`)
+})
+
+//const args2 = require('minimist')(process.args.slice(2));
+
+process.on('SIGTERM', ()=>{
+	server.close(()=>{
+		console.log("Process terminated");
+	});
+});
--- helloWorld.js
+++ helloWorld.js
@@ -6,4 +6,9 @@ const server = http.createServer((req,res) => {
     res.statusCode = 200
     res.setHeader('Content-Type', 'text/plain')
     res.end('Hello World!\n')
-})
+});
+
+//Kills the process instantly
+//process.exit();
+
+
